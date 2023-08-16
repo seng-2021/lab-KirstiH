d3adc8af0593eb37024420bb8eea8767ed862dca
kiharo <kirsti.haro@gmail.com>
2023-08-16 12:50:25 +0300

    Code corrected to pass the existing tests

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -4,21 +4,29 @@ def encode(s):
     if not isinstance(s,str):
         raise TypeError
     origlen = len(s)
+    s = s.ljust(1000, "a")
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
     for c in s:
+        if c in ['å', 'ä', 'ö']:
+            raise ValueError
         if c.isalpha():
             if c.islower():
                 c=c.upper()
+            else:
+                c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
+        else:
+            raise ValueError
+    
+    return crypted[:origlen]
 
 def decode(s):
-    return s
+    decoded = encode(s)
+    return decoded
 
